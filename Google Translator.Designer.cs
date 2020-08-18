8c9343cc32d4dbb45b959fd030a0df5f005ea111
Alexei <alexeiartyomov0206@gmail.com>
2020-08-18 14:59:37 +0300

    Initial Project

---
+++ Google Translator.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace GoogleTranslator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxPath = new System.Windows.Forms.TextBox();
+            this.buttonSelect = new System.Windows.Forms.Button();
+            this.comboBoxLang = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonTranslate = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Source File:";
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.Location = new System.Drawing.Point(136, 20);
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.ReadOnly = true;
+            this.textBoxPath.Size = new System.Drawing.Size(333, 29);
+            this.textBoxPath.TabIndex = 1;
+            // 
+            // buttonSelect
+            // 
+            this.buttonSelect.Location = new System.Drawing.Point(485, 20);
+            this.buttonSelect.Name = "buttonSelect";
+            this.buttonSelect.Size = new System.Drawing.Size(36, 30);
+            this.buttonSelect.TabIndex = 2;
+            this.buttonSelect.Text = "...";
+            this.buttonSelect.UseVisualStyleBackColor = true;
+            this.buttonSelect.Click += new System.EventHandler(this.buttonSelect_Click);
+            // 
+            // comboBoxLang
+            // 
+            this.comboBoxLang.FormattingEnabled = true;
+            this.comboBoxLang.Location = new System.Drawing.Point(184, 63);
+            this.comboBoxLang.Name = "comboBoxLang";
+            this.comboBoxLang.Size = new System.Drawing.Size(194, 32);
+            this.comboBoxLang.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(159, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Target Language:";
+            // 
+            // buttonTranslate
+            // 
+            this.buttonTranslate.Location = new System.Drawing.Point(396, 64);
+            this.buttonTranslate.Name = "buttonTranslate";
+            this.buttonTranslate.Size = new System.Drawing.Size(125, 32);
+            this.buttonTranslate.TabIndex = 5;
+            this.buttonTranslate.Text = "Translate";
+            this.buttonTranslate.UseVisualStyleBackColor = true;
+            this.buttonTranslate.Click += new System.EventHandler(this.buttonTranslate_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 121);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(528, 22);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(550, 166);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.buttonTranslate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBoxLang);
+            this.Controls.Add(this.buttonSelect);
+            this.Controls.Add(this.textBoxPath);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Google Translator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxPath;
+        private System.Windows.Forms.Button buttonSelect;
+        private System.Windows.Forms.ComboBox comboBoxLang;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonTranslate;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Google Translator.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GoogleTranslator
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<string, string> LANG_ID;
+        private List<string> AllOriginalText;
+
+        public Form1()
+        {
+            InitializeComponent();
+            LANG_ID = new Dictionary<string, string>();
+            AddLanguages();
+            AllOriginalText = new List<string>();
+            int Target = 3;
+            if ((Target & 2) == 2)
+                Console.WriteLine("10");
+            if ((Target & 1) == 1)
+                Console.WriteLine("01");
+        }
+
+        private void AddLanguages()
+        {
+            LANG_ID.Add("Afrikaans", "af");
+            LANG_ID.Add("Albanian", "sq");
+            LANG_ID.Add("Arabic", "ar");
+            LANG_ID.Add("Armenian", "hy");
+            LANG_ID.Add("Azerbaijani", "az");
+            LANG_ID.Add("Basque", "eu");
+            LANG_ID.Add("Belarusian", "be");
+            LANG_ID.Add("Bengali", "bn");
+            LANG_ID.Add("Bulgarian", "bg");
+            LANG_ID.Add("Catalan", "ca");
+            LANG_ID.Add("Chinese", "zh-CN");
+            LANG_ID.Add("Croatian", "hr");
+            LANG_ID.Add("Czech", "cs");
+            LANG_ID.Add("Danish", "da");
+            LANG_ID.Add("Dutch", "nl");
+            LANG_ID.Add("English", "en");
+            LANG_ID.Add("Esperanto", "eo");
+            LANG_ID.Add("Estonian", "et");
+            LANG_ID.Add("Filipino", "tl");
+            LANG_ID.Add("Finnish", "fi");
+            LANG_ID.Add("French", "fr");
+            LANG_ID.Add("Galician", "gl");
+            LANG_ID.Add("German", "de");
+            LANG_ID.Add("Georgian", "ka");
+            LANG_ID.Add("Greek", "el");
+            LANG_ID.Add("Haitian Creole", "ht");
+            LANG_ID.Add("Hebrew", "iw");
+            LANG_ID.Add("Hindi", "hi");
+            LANG_ID.Add("Hungarian", "hu");
+            LANG_ID.Add("Icelandic", "is");
+            LANG_ID.Add("Indonesian", "id");
+            LANG_ID.Add("Irish", "ga");
+            LANG_ID.Add("Italian", "it");
+            LANG_ID.Add("Japanese", "ja");
+            LANG_ID.Add("Korean", "ko");
+            LANG_ID.Add("Lao", "lo");
+            LANG_ID.Add("Latin", "la");
+            LANG_ID.Add("Latvian", "lv");
+            LANG_ID.Add("Lithuanian", "lt");
+            LANG_ID.Add("Macedonian", "mk");
+            LANG_ID.Add("Malay", "ms");
+            LANG_ID.Add("Maltese", "mt");
+            LANG_ID.Add("Norwegian", "no");
+            LANG_ID.Add("Persian", "fa");
+            LANG_ID.Add("Polish", "pl");
+            LANG_ID.Add("Portuguese", "pt");
+            LANG_ID.Add("Romanian", "ro");
+            LANG_ID.Add("Russian", "ru");
+            LANG_ID.Add("Serbian", "sr");
+            LANG_ID.Add("Slovak", "sk");
+            LANG_ID.Add("Slovenian", "sl");
+            LANG_ID.Add("Spanish", "es");
+            LANG_ID.Add("Swahili", "sw");
+            LANG_ID.Add("Swedish", "sv");
+            LANG_ID.Add("Tamil", "ta");
+            LANG_ID.Add("Telugu", "te");
+            LANG_ID.Add("Thai", "th");
+            LANG_ID.Add("Turkish", "tr");
+            LANG_ID.Add("Ukrainian", "uk");
+            LANG_ID.Add("Urdu", "ur");
+            LANG_ID.Add("Vietnamese", "vi");
+            LANG_ID.Add("Welsh", "cy");
+            LANG_ID.Add("Yiddish", "yi");
+
+            for (int i = 0; i < LANG_ID.Count; i++)
+                comboBoxLang.Items.Add(LANG_ID.ElementAt(i).Key);
+        }
+
+
+
+        private string GetFilePathWithOpenDialog()
+        {
+            string filePath = "";
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "C:\\Surferbuddy";
+                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                //openFileDialog.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+                }
+            }
+            return filePath;
+        }
+
+        private void buttonSelect_Click(object sender, EventArgs e)
+        {
+            textBoxPath.Text = GetFilePathWithOpenDialog();
+        }
+
+        private void buttonTranslate_Click(object sender, EventArgs e)
+        {
+            if (textBoxPath.Text == "" || comboBoxLang.Text == "")
+                return;
+            LoadTranslateDataFromFile(textBoxPath.Text);
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = AllOriginalText.Count;
+            GenerateGoogleTranslationFile(comboBoxLang.Text, textBoxPath.Text);
+            progressBar1.Value = 0;
+        }
+
+        private void GenerateGoogleTranslationFile(string targetLang, string path)
+        {
+            string file = Path.GetDirectoryName(path) + "/" + targetLang + ".txt";
+            File.WriteAllText(file, string.Empty); // Clear content before writing
+            foreach (var orgText in AllOriginalText)
+            {
+                using (StreamWriter sw = File.AppendText(file))
+                {
+                    sw.WriteLine("msgid:\t" + orgText.Trim());
+                    string transText = "";
+                    if (targetLang != "English")
+                    {
+                        transText = GoogleTranslate(orgText, "English", targetLang);
+                    }
+                    if(transText == "")
+                    {
+                        MessageBox.Show("Too many request. Please try it later");
+                        break;
+                    }
+                    sw.WriteLine("msgstr:\t" + transText.Trim());
+                    sw.WriteLine("");
+                    progressBar1.Value++;
+                }
+            }
+            MessageBox.Show("Google Translation Finished");
+        }
+
+        private void LoadTranslateDataFromFile(string file)
+        {
+            try
+            {
+                string[] lines = File.ReadAllLines(file);
+                string msgid, msgstr;
+                AllOriginalText.Clear();
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    if (lines[i] == "") continue;
+                    if (lines[i].Substring(0, Math.Min(lines[i].Length, 6)) == "msgid:"
+                        && i < lines.Length - 1
+                        && lines[i + 1].Substring(0, Math.Min(lines[i + 1].Length, 7)) == "msgstr:")
+                    {
+                        msgid = lines[i].Substring(6).Trim();
+                        //msgstr = lines[i + 1].Substring(7).Trim();
+                        AllOriginalText.Add(msgid);
+                        continue;
+                    }
+                }
+            }
+            catch (FileNotFoundException e)
+            {
+                MessageBox.Show(string.Format("Cannot read Translation file - {0}", e.FileName));
+            }
+        }
+
+        public string GoogleTranslate
+                (string sourceText,
+                 string sourceLanguage,
+                 string targetLanguage)
+        {
+            // Initialize
+            string translation = string.Empty;
+            try
+            {
+                // Download translation
+                string url = string.Format("https://translate.googleapis.com/translate_a/single?client=gtx&sl={0}&tl={1}&dt=t&q={2}",
+                                            LANG_ID[sourceLanguage],
+                                            LANG_ID[targetLanguage],
+                                            sourceText);
+                string outputFile = Path.GetTempFileName();
+                using (WebClient wc = new WebClient())
+                {
+                    wc.Headers.Add("user-agent", "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36");
+                    wc.DownloadFile(url, outputFile);
+                }
+
+                // Get translated text
+                if (File.Exists(outputFile))
+                {
+
+                    // Get phrase collection
+                    string text = File.ReadAllText(outputFile);
+                    int index = text.IndexOf(string.Format(",,\"{0}\"", LANG_ID[sourceLanguage]));
+                    if (index == -1)
+                    {
+                        // Translation of single word
+                        int startQuote = text.IndexOf('\"');
+                        if (startQuote != -1)
+                        {
+                            int endQuote = text.IndexOf('\"', startQuote + 1);
+                            if (endQuote != -1)
+                            {
+                                translation = text.Substring(startQuote + 1, endQuote - startQuote - 1);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        // Translation of phrase
+                        text = text.Substring(0, index);
+                        text = text.Replace("],[", ",");
+                        text = text.Replace("]", string.Empty);
+                        text = text.Replace("[", string.Empty);
+                        text = text.Replace("\",\"", "\"");
+
+                        // Get translated phrases
+                        string[] phrases = text.Split(new[] { '\"' }, StringSplitOptions.RemoveEmptyEntries);
+                        for (int i = 0; (i < phrases.Count()); i += 2)
+                        {
+                            string translatedPhrase = phrases[i];
+                            if (translatedPhrase.StartsWith(",,"))
+                            {
+                                i--;
+                                continue;
+                            }
+                            translation += translatedPhrase + "  ";
+                        }
+                    }
+
+                    // Fix up translation
+                    translation = translation.Trim();
+                    translation = translation.Replace(" ?", "?");
+                    translation = translation.Replace(" !", "!");
+                    translation = translation.Replace(" ,", ",");
+                    translation = translation.Replace(" .", ".");
+                    translation = translation.Replace(" ;", ";");
+
+                    // And translation speech URL
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            // Return result
+            return translation;
+        }
+    }
+}
